2aebaa080c0167cf0b1e9ae051d8e75e9fada2f1
maximdorogov <maxim_dorogov@yahoo.com>
2018-05-26 16:38:59 -0300

    Add files via upload

---
+++ UART/test_serial.ino
@@ -0,0 +1,20 @@
+void setup() {
+  Serial.begin(115200);
+
+}
+int i = 0;
+int datos;
+void loop() {
+
+  if(Serial.available()>0)
+  {
+     
+      datos = Serial.read();
+      //delay(2); 
+      Serial.write(datos);
+      
+    
+  }
+
+    
+}
